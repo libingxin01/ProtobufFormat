33e72cee53391dbc68514755af03fe3de62aa452
Green Lee <bkingli1984@gmail.com>
2017-07-06 11:45:55 +0800

    v0.1

---
+++ ProtobufFormat/User.cs
@@ -0,0 +1,15 @@
+﻿using ProtoBuf;
+
+namespace ProtobufFormat
+{
+    [ProtoContract]
+    public class User
+    {
+        [ProtoMember(1)]
+        public int Id { get; set; }
+        [ProtoMember(2)]
+        public string Name { get; set; }
+        [ProtoMember(3)]
+        public int Age { get; set; }
+    }
+}
